3b601e7b2404db8b34a34a617ac89b7a3e77b3da
robin <rcapra@talentsoft.com>
2022-09-26 09:48:44 +0200

    service worker

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from 'react-query'
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from './App';
 
 const queryClient = new QueryClient()
@@ -16,4 +16,10 @@ root.render(
     </QueryClientProvider>
   </React.StrictMode>
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
